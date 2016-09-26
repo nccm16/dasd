c1496dc3710e455b31efa627ad3e2c049e321fbf
nccm16 <nccm16@gmail.com>
2016-09-26 09:57:41 -0700

    Create ss.js

---
+++ ss.js
@@ -0,0 +1,73 @@
+<script>
+function addValue1()
+{
+    var value = parseInt(document.getElementById('number1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value + 10;
+    document.getElementById('number1').value = value;
+}
+function reduceValue1()
+{
+    var value = parseInt(document.getElementById('number1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number1').value = value;
+}
+function reduceValue2()
+{
+    var value = parseInt(document.getElementById('number2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number2').value = value;
+}
+function reduceValue3()
+{
+    var value = parseInt(document.getElementById('number3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number3').value = value;
+}
+function reduceValue4()
+{
+    var value = parseInt(document.getElementById('number4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number4').value = value;
+}
+function reduceValue5()
+{
+    var value = parseInt(document.getElementById('number5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number5').value = value;
+}
+function reduceValue6()
+{
+    var value = parseInt(document.getElementById('number6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number6').value = value;
+}
+function reduceValue7()
+{
+    var value = parseInt(document.getElementById('number7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number7').value = value;
+}
+function reduceValue8()
+{
+    var value = parseInt(document.getElementById('number8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number8').value = value;
+}
+function reduceValue9()
+{
+    var value = parseInt(document.getElementById('number9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value = value - 10;
+    document.getElementById('number9').value = value;
+}
+   
+</script>
